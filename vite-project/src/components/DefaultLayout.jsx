97a4a5f8587ca966f9ce5353f9426c30fbc629f5
Daniel Popescu <same.dan@gmail.com>
2023-01-18 15:55:17 +0100

    Vite settings to autoload on save

---
+++ vite-project/src/components/DefaultLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
+
+export default function DefaultLayout() {
+    const { user, token } = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login" />;
+    }
+
+    return (
+        <div>
+            DefaultLayout HIOHJOIPJ
+            <Outlet />
+        </div>
+    );
+}
---
+++ vite-project/src/components/GuestLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+export default function GuestLayout() {
+    return (
+        <div>
+            GuestLayoutH O OII
+            <Outlet />
+        </div>
+    );
+}
