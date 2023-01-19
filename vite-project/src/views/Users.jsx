3a743ce2edad12173a195df0a9dea0504dbe605c
Daniel Popescu <same.dan@gmail.com>
2023-01-19 16:52:38 +0100

    Show Users on FEnd

--- vite-project/src/views/Users.jsx
+++ vite-project/src/views/Users.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axiosClient from "./../axios-client";
 
 export default function Users() {
@@ -14,11 +15,62 @@ export default function Users() {
         axiosClient
             .get("/users")
             .then(({ data }) => {
+                setUsers(data.data);
                 setLoading(false);
                 console.log(data);
             })
             .catch(() => setLoading(false));
     };
 
-    return <div>Users</div>;
+    return (
+        <div>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                }}
+            >
+                <h1>Users</h1>
+                <Link to="/users/new" className="btn-add">
+                    Add new user
+                </Link>
+            </div>
+            <div className="card animated fadeInDown">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Create Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((u) => (
+                            <tr>
+                                <td>{u.id}</td>
+                                <td>{u.name}</td>
+                                <td>{u.email}</td>
+                                <td>{u.created_at}</td>
+                                <td>
+                                    <Link
+                                        to={"/users/" + u.id}
+                                        className="btn-edit"
+                                    >
+                                        Edit
+                                    </Link>
+                                    &nbsp;
+                                    <button className="btn-delete">
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
